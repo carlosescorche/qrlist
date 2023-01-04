17eedc216d32cb6fc575cb6e9b6493dc7a0df66b
Carlos Escorche <carlosescorchep@gmail.com>
2023-01-04 08:21:13 -0300

    Add consts and types

---
+++ src/types.d.ts
@@ -0,0 +1,14 @@
+type List = {
+  id: string;
+  name: string;
+  description: string;
+  code: string;
+};
+
+type Subscription = {
+  id: string;
+  listId: string;
+  number: string;
+  status: string;
+  createdAt: string;
+};
