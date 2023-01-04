2b9810e0a94ae6fdedfa504fe4139ee980488012
Carlos Escorche <carlosescorchep@gmail.com>
2023-01-04 08:19:11 -0300

    Adds App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import List from "./pages/List";
+import Signup from "./pages/Signup";
+import Subscription from "./pages/Subscription";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/list/:id" element={<List />} />
+        <Route path="/list/:id/signup" element={<Signup />} />
+        <Route path="/subscription/:id" element={<Subscription />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
